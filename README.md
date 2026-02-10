b3811577760b2aeb2472c29c94d402b31a0d1eb3
Nick666handsome <hsaiohubert@gmail.com>
2026-02-10 05:05:19 +0000

    Add Vite React setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# nick-family-food
-Repository name: nick-family-food Description: 家庭點餐系統 - 類似 foodpanda 的即時點餐平台
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🚀 FamilyFood 家庭點餐系統</h1>
+      <p>網站部署成功！Vercel 正在重新部署...</p>
+    </div>
+  )
+}
+
+export default App
